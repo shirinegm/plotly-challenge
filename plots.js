90d6be5e37fa2d352e2a04f7ceecc6ce71537266
Shi Mas <shirinegm@gmail.com>
2022-01-08 18:51:39 -0600

    Added bubble chart using colorscale

--- plots.js
+++ plots.js
@@ -5,11 +5,97 @@ console.log("plots.js loaded!");
 // Define function to draw Bar Chart
 function drawBarChart(sampleId) {
     console.log(`drawBarChart(${sampleId})`);
+
+    // Read the data
+    d3.json("samples.json").then(data => {
+
+        let samples = data.samples;
+
+        // Get only the result matching what was passed to the function
+        let resultArray = samples.filter(s => s.id === sampleId);
+        let result = resultArray[0];
+
+        // Get handles for all the data necessary to draw the chart
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        // Map out Sample Ids to show as labels on the Y axis
+        let yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        // Define the bar data traces
+        let barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
+        }
+
+        // Add the trace into an array
+        let barArray = [barData];
+
+        // Add a layout definition
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures by Sample",
+            margin: { t:100, l: 150}
+
+        }
+
+        // Call Plotly on the trace array
+        Plotly.newPlot("bar", barArray, barLayout);
+    });
 }
 
 // Define function to draw Bubble Chart
 function drawBubbleChart(sampleId) {
     console.log(`drawBubbleChart(${sampleId})`);
+    // Read the data
+    d3.json("samples.json").then(data => {
+
+        let samples = data.samples;
+
+        // Get only the result matching what was passed to the function
+        let resultArray = samples.filter(s => s.id === sampleId);
+        let result = resultArray[0];
+
+        // Get handles for all the data necessary to draw the chart
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        // Map out Sample Ids to show as labels on the Y axis
+        let yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        // Define the bar data traces
+        let bubbleData = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: 'Earth'
+            },
+            text: otu_labels,
+
+        };
+
+        // Add the trace into an array
+        let bubbleArray = [bubbleData];
+
+        // Add a layout definition
+        let bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            showlegend: false,
+            margin: { t:100, l: 150}
+
+        }
+
+        // Call Plotly on the trace array
+        Plotly.newPlot("bubble", bubbleArray, bubbleLayout);
+    });
+
 }
 
 // Define function to populate Demographic Data
